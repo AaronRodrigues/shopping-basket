727fdf1edfebf38b5dd10fdd8c2ab2c5d72e7ae5
AaronRodrigues <aaronrod@gmail.com>
2021-11-08 21:35:26 +0000

    Add test and NUnit

---
+++ shopping-basket.tests/ShoppingBasketTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace shopping_basket.tests
+{
+    public class ShoppingBasketTests
+    {
+        [Test]
+        public void ShouldAddOneItemToShoppingBasket()
+        {
+            var shoppingBasket = new ShoppingBasket();
+            shoppingBasket.Add("Bread");
+            Assert.That(shoppingBasket.Total(), Is.EqualTo(1));
+        }
+    }
+}
---
+++ shopping-basket/ShoppingBasket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace shopping_basket
+{
+    public class ShoppingBasket : IShoppingBasket
+    {
+        public void Add(string item)
+        {
+           
+        }
+
+        public int Total()
+        {
+            return 1;
+        }
+    }
+
+    public interface IShoppingBasket
+    {
+        void Add(string item);
+        int Total();
+    }
+}
